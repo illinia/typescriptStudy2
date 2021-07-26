99f99ada72144cfeb05ed13ac87686beb575ad72
illinia <67290425+illinia@users.noreply.github.com>
2021-07-26 23:28:39 +0900

    1~27

---
+++ chapter-3/src/index.ts
@@ -0,0 +1,43 @@
+// function squareOf(n: number) {
+//   return n * n;
+// }
+// squareOf(2);
+// squareOf("z");
+
+// let a: any = 666;
+// let b: any = ["danger"];
+// let c = a + b;
+
+// let a: unknown = 30;
+// let b = a === 123;
+// let c = a + 10;
+// if (typeof a === "number") {
+//   let d = a + 10;
+// }
+
+// let a = true
+// var b = false
+// const c = true;
+// let d: boolean = true;
+// let e: true = true;
+// let f: true = false;
+
+// let a = 1234;
+// var b = Infinity * 0.10;
+// const c = 5678;
+// let d = a < b;
+// let e: number = 100;
+// let f: 26.218 = 26.218;
+// let g: 26.218 = 10;
+
+// let oneMillion = 1_000_000
+// let twoMillion: 2_000_000 = 2_000_000;
+
+// let a = 1234n;
+// const b = 5678n;
+// var c = a + b;
+// let d = a < 1235;
+// // let e = 88.5n;
+// let f: bigint = 100n;
+// let g: 100n = 100n;
+// let h: bigint = 100;
